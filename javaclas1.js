38822f5394c22d6f8e1b2c71105ec1cd043e356e
naveenswargam <naveenswargam2018@gmail.com>
2023-09-23 20:34:42 +0530

    java class

---
+++ javaclas1.js
@@ -0,0 +1,101 @@
+Number
+string 
+Boolean
+
+Number
+let num1=34430
+let num2=3443
+total=num1+num2
+console.log(num1.length)
+console.log(total.length)
+let length=total.length
+
+let num = "hfjkaslfkffjfj"
+let length = num.length;
+console.log(num.length)
+
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+console.log(text.length)
+
+var num1="Naveen"
+if(true){
+     num2="Raghu"
+  console.log("inside",num2)
+}
+ console.log("outside",num1)
+
+ var num1="Naveen"
+ num2="Raghu"
+ console.log(num2)
+
+
+
+
+String topic
+
+
+let name1="naveen"
+let name2="raghu"
+let name3="laxmi"
+console.log(name1+" "+name2+" "+name3)
+
+let total=(name1+" "+name2+" "+name3)
+let total=(name1+ name2+ name3)
+
+let name1="naveen"
+let name2= "9595"
+let name3="laxmi"
+console.log(name1+" "+name2+" "+name3)
+console.log((name1+" "+name2+" "+name3).length)
+
+let length=name2.length
+console.log(length)
+
+
+
+Number topic
+
+let num1=2333
+let num2=8585
+let total=num1+num2
+console.log(total)
+
+
+
+slice topic
+
+let text="apple,mango,banana,straberry,pineapple,juice"
+let text1=text.slice(-5,-1)
+console.log(text1)
+
+
+ let text="slice() extracts a part of a string and returns the extracted part in a new string.";
+ let data= text.slice(-14,-1);
+ let length=data.length
+ console.log(length)
+ console.log(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
