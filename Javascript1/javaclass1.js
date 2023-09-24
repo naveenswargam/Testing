ffca31b3046f16ce0b76ac4df365fb2f08f475d6
naveenswargam <naveenswargam2018@gmail.com>
2023-09-24 20:48:57 +0530

    added file java1

---
+++ Javascript1/javaclass1.js
@@ -0,0 +1,155 @@
+// // Number
+// // string 
+// // Boolean
+
+// // Number
+// // let num1=34430
+// // let num2=3443
+// // total=num1+num2
+// // console.log(num1.length)
+// // console.log(total.length)
+// // let length=total.length
+
+// // let num = "hfjkaslfkffjfj"
+// // let length = num.length;
+// // console.log(num.length)
+
+
+// // let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// // let length = text.length;
+// // console.log(text.length)
+
+// // var num1="Naveen"
+// // if(true){
+// //      num2="Raghu"
+// //   console.log("inside",num2)
+// // }
+// //  console.log("outside",num1)
+
+// //  var num1="Naveen"
+// //  num2="Raghu"
+// //  console.log(num2)
+
+
+
+
+// // String topic
+
+
+// // let name1="naveen"
+// // let name2="raghu"
+// // let name3="laxmi"
+// // console.log(name1+" "+name2+" "+name3)
+
+// // let total=(name1+" "+name2+" "+name3)
+// // let total=(name1+ name2+ name3)
+
+// // let name1="naveen"
+// // let name2= "9595"
+// // let name3="laxmi"
+// // console.log(name1+" "+name2+" "+name3)
+// // console.log((name1+" "+name2+" "+name3).length)
+
+// // let length=name2.length
+// // console.log(length)
+
+
+
+// //Number topic
+
+// // let num1=2333
+// // let num2=8585
+// // let total=num1+num2
+// // console.log(total)
+
+
+
+// // slice topic
+
+// // let text="apple,mango,banana,straberry,pineapple,juice"
+// // let text1=text.slice(-5,-1)
+// // console.log(text1)
+
+
+// //  let text="slice() extracts a part of a string and returns the extracted part in a new string.";
+// //  let data= text.slice(-14,-1);
+// //  let length=data.length
+// //  console.log(length)
+// //  console.log(data)
+
+// // substring
+
+// // var text1="apple,mango,banana,straberry,pineapple,juice"
+// // var text2="This is the front page of the Simple English Wikipedia."
+// // var text3=text1.substring(5,10)  
+// // var text4=text2.substring(10,20)
+// // var text5=(((text1+text2).length);
+// // var text5=((text3+text4).length);
+
+// // console.log(text3,(text1+text2).length)
+// // console.log(text4,(text3+text4).length)
+// // console.log(text4)
+// // console.log(text5)
+
+// // const text1="apple,mango,banana,straberry,pineapple,juice"
+// // const text1="This is the front page of the Simple English Wikipedia."
+// // const text3=text1.substring(5,10)  
+// // const text4=text1.substring(10,20)
+
+// // console.log(text3,(text1+text1).length)
+// // console.log(text4,(text3+text4).length)
+
+
+// // (num is a declearation)  (33993 is a assign) (declear and assign both are possible)
+// // var  num=33993   
+// // var  num=33993
+// // console.log(num+num)
+
+// // var  num="naveen" 
+// //  var num1="raghu"
+// // console.log(num+num1)
+
+
+
+
+// // let  (only declearation is not possible and assign is possible)
+// // let  num=33993   
+// // let  num1=33993
+// // console.log(num+num1)
+
+// // let  num="naveen" 
+// //   num="raghu"
+// // console.log(num)
+
+
+
+
+ // (No decleared and No assign in const)
+ // constant  
+// const num=33993   
+// // const num1=33993
+// // console.log(num+num1)
+
+// const num="ABCD" 
+//  num1="abcd"
+// console.log(num+num1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
