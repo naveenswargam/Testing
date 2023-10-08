49bfe1fd2342836efec636aca5728fa4642a4220
naveenswargam <naveenswargam2018@gmail.com>
2023-10-08 20:55:53 +0530

    added files

---
+++ java1.js
@@ -0,0 +1,50 @@
+// Number
+// string 
+// Boolean
+// slice 
+// substring
+// supstring
+// toUppercase
+// tolowercase
+// var 
+// let
+// constant-const
+// double quotes
+// single quotes
+
+
+// number
+
+// let num1=34430
+// let num2=3443
+// total=num1+num2
+// console.log(num1+num2);
+
+// let data1="naveen"
+// let data="suman,naveen,shyam,raju"
+// console.log(data)
+// console.log(typeof(data1))
+
+
+// let sum1=4444
+// let sum2=85958
+// console.log(typeof(sum1+sum2))
+
+// let data1=3333
+// let data2="naveen"
+// let length=data1.length
+// console.log(length)
+
+// let data="(5..5),(7.)"
+// let data=data.slice
+// console.log(data)
+
+
+
+
+
+
+
+
+
+
--- javaclass1.js
+++ javaclass1.js
@@ -77,6 +77,69 @@
 //  console.log(length)
 //  console.log(data)
 
+// substring
+
+// var text1="apple,mango,banana,straberry,pineapple,juice"
+// var text2="This is the front page of the Simple English Wikipedia."
+// var text3=text1.substring(5,10)  
+// var text4=text2.substring(10,20)
+// var text5=(((text1+text2).length);
+// var text5=((text3+text4).length);
+
+// console.log(text3,(text1+text2).length)
+// console.log(text4,(text3+text4).length)
+// console.log(text4)
+// console.log(text5)
+
+// const text1="apple,mango,banana,straberry,pineapple,juice"
+// const text1="This is the front page of the Simple English Wikipedia."
+// const text3=text1.substring(5,10)  
+// const text4=text1.substring(10,20)
+
+// console.log(text3,(text1+text1).length)
+// console.log(text4,(text3+text4).length)
+
+
+// (num is a declearation)  (33993 is a assign) (declear and assign both are possible)
+// var  num=33993   
+// var  num=33993
+// console.log(num+num)
+
+// var  num="naveen" 
+//  var num1="raghu"
+// console.log(num+num1)
+
+
+
+
+// let  (only declearation is not possible and assign is possible)
+// let  num=33993   
+// let  num1=33993
+// console.log(num+num1)
+
+// let  num="naveen" 
+//   num="raghu"
+// console.log(num)
+
+
+
+
+//  (No decleared and No assign in const)
+//  constant  
+// const num=33993   
+// const num1=33993
+// console.log(num+num1)
+
+// const num="ABCD" 
+//  num1="abcd"
+// console.log(num+num1)
+
+
+
+// let  num="naveen" 
+//   num="raghu"
+// console.log(num)
+
 
 
 
