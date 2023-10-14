32ee98d6b983cd62eb962e6ad16013c79d50beac
naveenswargam <naveenswargam2018@gmail.com>
2023-10-14 17:58:40 +0530

    java practice files

--- Javascript1/practice.js
+++ Javascript1/practice.js
@@ -1,111 +1,187 @@
 
-var    (it is possible)
-let   (Identifier 'name' has already been declared) 
-constant   (Identifier 'x' has already been declared)
-boolean (true or false)
+// var    (it is possible)
+// let   (Identifier 'name' has already been declared) 
+// constant   (Identifier 'x' has already been declared)
+// boolean (true or false)
 
-var name="naveen"
-name="shyam"
-console.log(name)
+// var name="naveen"
+// name="shyam"
+// console.log(name)
 
-var name1="naveen"
-var name="shyam"
-total=(name1+" "+name)
-console.log(total)
+// var name1="naveen"
+// var name="shyam"
+// total=(name1+" "+name)
+// console.log(total)
 
-var name="naveen"
-var name="shyam"
-total=(name+" "+name)
-console.log(total)
+// var name="naveen"
+// var name="shyam"
+// total=(name+" "+name)
+// console.log(total)
 
-var number1=9696
-var number="naveen"
-total=(number1+"                "+    number)
-console.log(total)
+// var number1=9696
+// var number="naveen"
+// total=(number1+"                "+    number)
+// console.log(total)
 
-let
+// let
 
-let name="naveen"
-let name="shyam"
-total=(name+" "+name)
-console.log(total)
+// let name="naveen"
+// let name="shyam"
+// total=(name+" "+name)
+// console.log(total)
 
-constant
+// constant
 
-const x=5
-const y=6
-total=x+y
-console.log(total)
+// const x=5
+// const y=6
+// total=x+y
+// console.log(total)
 
-const x=5
-const y=6
-total=x+y
-console.log(total)
+// const x=5
+// const y=6
+// total=x+y
+// console.log(total)
 
-const x=5
-const x=6
-total=x+x
-console.log(total)
-
-
-
-let x = 5;
-let y = 5;
-let z = (x+y)
-console.log(z)
-console.log(typeof(z))
+// const x=5
+// const x=6
+// total=x+x
+// console.log(total)
 
 
 
-let x = "true";
-let y = "false";
-total=x+y
-console.log(typeof(total))
-
-empty value
-let x=""
-let y="0"
-console.log(x.typeof(x))
-console.log(y.typeof(y))
-
-single and double quotations
-
-var name="my name s.naveen working professional's"
-var name1='my name s.naveen working professional"s'
-console.log(name)
-console.log(name1)
-
-var name="my name s.naveen working 'professional's"
-var name1='my name s.naveen "working" professionals'
-var name2="my  "
-var name3="s naveen"
-console.log(name)
-console.log(name1)
-console.log(name.length)
-console.log(name2.length)
-console.log(name3.length)
-
-var name=("my name s.naveen working \'professional\'s")
-var name1='my name s.naveen \"working\" professionals'
-console.log(name)
-console.log(name.length)
-console.log(name1)
+// let x = 5;
+// let y = 5;
+// let z = (x+y)
+// console.log(z)
+// console.log(typeof(z))
 
 
-back tick
 
-var name=`my name s.naveen working professionals`
-var name1=`my name s.naveen "working" professionals`
-console.log(name)
-console.log(typeof(name1))
+// let x = "true";
+// let y = "false";
+// total=x+y
+// console.log(typeof(total))
+
+// empty value
+// let x=""
+// let y="0"
+// console.log(x.typeof(x))
+// console.log(y.typeof(y))
+
+// single and double quotations
+
+// var name="my name s.naveen working professional's"
+// var name1='my name s.naveen working professional"s'
+// console.log(name)
+// console.log(name1)
+
+// var name="my name s.naveen working 'professional's"
+// var name1='my name s.naveen "working" professionals'
+// var name2="my  "
+// var name3="s naveen"
+// console.log(name)
+// console.log(name1)
+// console.log(name.length)
+// console.log(name2.length)
+// console.log(name3.length)
+
+// var name=("my name s.naveen working \'professional\'s")
+// var name1='my name s.naveen \"working\" professionals'
+// console.log(name)
+// console.log(name.length)
+// console.log(name1)
 
 
-string length
+// back tick
 
-var name=("my name s.naveen working professionals")
-console.log(name.length)
+// var name=`my name s.naveen working professionals`
+// var name1=`my name s.naveen "working" professionals`
+// console.log(name)
+// console.log(typeof(name1))
 
-slice
+
+// string length
+
+// var name=("my name s.naveen working professionals")
+// console.log(name.length)
+
+// slice (it is comes from after given number)
+
+// let text="Apple, banana, kiwi";
+// let part=text.slice(4,15)
+// let part=text.slice(-1)
+// let part=text.slice(-4)
+// console.log(part)
+
+// sub string  (it is count from 1)
+//  let str="naveen shyam raju"
+//  let part=str.substring(7,15)
+//  console.log(part)
+
+// let str="naveen shyam raju"
+// let part1=str.slice(5,15)
+// let part2=str.substr(7,15)
+// console.log(part1.part2)
+
+// console.log(part1)
+// console.log(part2)
+
+
+
+// replace and replace all
+
+// let text="i am a bad boy"
+// const text2=text.replace("bad","good")
+// console.log(text2)
+
+// var name="naveen shyam shyam shyam shyam shyam"
+// var name2=name.replaceAll("shyam","naveen")
+// console.log(name2)
+
+// var name="naveen shyam shyam shyam shyam shyam"
+// var name2=name.replace(/shyam/g,"naveen")
+// console.log(name2)
+
+
+
+// i=case sensitive
+
+// let text="i am a bad boy" 
+// const text2=text.replace(/Bad/i,"good")
+// console.log(text2)
+
+// g=global
+
+// let text="i am a bad boy bad boy bad boy bad boy" 
+// const text2=text.replace(/bad/g,"good")
+// console.log(text2)
+
+//ig
+
+// let text1="i am a bad boy bad boy bad boy bad boy" 
+// const text3=text1.replace(/Bad/ig,"good")
+// console.log(text3)
+
+// uppercase and lowercase
+// let text1="Hello World"
+// let text2="HELLO WORLD"
+// let text3=text1.toUpperCase();
+// let text4=text2.toLowerCase();
+// console.log(text3)
+// console.log(text4)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
